9af153292f39cd8a32ff45070b2258d67f731bd3
Andrey Zaytcev <andrey@hypemasters.com>
2021-12-05 14:21:39 +0300

    Загрузка базы данных по кнопке

--- Assets/Scripts/SO/ItemDBSO.cs
+++ Assets/Scripts/SO/ItemDBSO.cs
@@ -1,8 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 [CreateAssetMenu(fileName = "ItemDatabase", menuName = "Data/Data/ItemDatabase", order = 4)]
 public class ItemDBSO : ScriptableObject
 {
     public List<ItemDataSO> ItemsData = new List<ItemDataSO>();
+
+    public void RefreshDatabase()
+    {
+        ItemsData.Clear();
+        
+        var guids = AssetDatabase.FindAssets("t:ItemDataSO");
+        foreach (string guid in guids)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            ItemsData.Add(AssetDatabase.LoadAssetAtPath<ItemDataSO>(path));
+            Debug.Log("Add item to data base: " + path);
+        }
+    }
+}
+
+[CustomEditor(typeof(ItemDBSO))]
+public class ItemDbsoEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ItemDBSO myScript = (ItemDBSO)target;
+        if(GUILayout.Button("Refresh Database"))
+        {
+            myScript.RefreshDatabase();
+        }
+    }
 }
